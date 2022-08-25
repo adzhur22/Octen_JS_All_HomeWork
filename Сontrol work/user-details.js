1d5147ae67efc438f119aa7640fa7eefd927daf8
adzhur22 <boris.web.ad@gmail.com>
2022-08-25 06:43:33 +0200

    add map

--- Сontrol work/user-details.js
+++ Сontrol work/user-details.js
@@ -6,8 +6,13 @@
 
 let url = new URL(location.href);
 let idOfUser = url.searchParams.get('id');
-let i = 0;
+
 let divUserContent = document.getElementsByClassName('userContent')[0];
+ let fullContent = document.getElementsByClassName('fullContent')[0];
+
+ let i = 0;
+ let i2 = 0;
+
 
 
 fetch(`https://jsonplaceholder.typicode.com/users/${idOfUser}`)
@@ -15,13 +20,10 @@ fetch(`https://jsonplaceholder.typicode.com/users/${idOfUser}`)
     .then((user) => {
 
         // вирішив я рекурсію тут спробувати так як там є об'єкти в середині яких інші об'єкти.
-        //
+        // хоч можна було по іншому але чому б і ні)
 
 
-
-        console.log(user);
-
-        function reader (object, lastObjectKay) {
+        function reader (object, lastObjectKay) { // змінна lastObjectKay, це ім'я обєкта в середині якого є інший обєкт. Я реалізував вичавлення значення цієї змінної щоб одразу добавити її як назву новоствореному блоку з даними.
             if (typeof object === 'object'){
 
                 let block = document.createElement("div");
@@ -30,8 +32,10 @@ fetch(`https://jsonplaceholder.typicode.com/users/${idOfUser}`)
                 divUserContent.appendChild(block);
 
                 if(lastObjectKay){
+
                     let addHead = document.createElement('div');
                     addHead.innerHTML = `<h4>${lastObjectKay}</h4>`;
+                    addHead.classList.add(lastObjectKay);
                     block.appendChild(addHead);
                     block.id = lastObjectKay;
                 }
@@ -40,16 +44,45 @@ fetch(`https://jsonplaceholder.typicode.com/users/${idOfUser}`)
                 for (const objectKey in object) {
 
 
-                    let pointInformation = document.createElement("div");
-                    pointInformation.classList.add('pointInformation');
+                    if (typeof object[objectKey] !== 'object' && lastObjectKay !== 'geo') {
 
+                        let pointInformation = document.createElement("div");
+                        pointInformation.classList.add('pointInformation');
 
-
-                    if (typeof object[objectKey] !== 'object') {
-                        console.log(objectKey, object[objectKey]);
-                        pointInformation.innerHTML = `${objectKey}: ${object[objectKey]}`
+                        pointInformation.innerHTML = `${objectKey}: ${object[objectKey]}`;
                         block.appendChild(pointInformation);
 
+                    }
+                    if (typeof object[objectKey] !== 'object' && lastObjectKay === 'geo' && i2 === 0) {
+
+                        let mapa = document.getElementById('geo');
+                        console.log(mapa);
+
+                        let divMap = document.createElement('div');
+                        divMap.id = 'map';
+                        mapa.appendChild(divMap);
+
+                        let lat = parseFloat(object.lat);
+                        let lng = parseFloat(object.lng);
+                        console.log(lat, lng);
+
+                        function initMap() {
+
+                            const uluru = { lat: lat, lng: lng};
+
+                            const map = new google.maps.Map(document.getElementById("map"), {
+                                zoom: 1,
+                                center: uluru,
+                            });
+
+                            const marker = new google.maps.Marker({
+                                position: uluru,
+                                map: map,
+                            });
+                        }
+
+                        initMap();
+                        i2++;
                     }
 
                     else{
@@ -65,9 +98,60 @@ fetch(`https://jsonplaceholder.typicode.com/users/${idOfUser}`)
     });
 
 
-let userContentDown = document.getElementsByClassName('userContentDown');
+let userContentDown = document.getElementsByClassName('userContentDown')[0];
 let but = document.createElement('button');
+but.classList.add('button2');
+but.innerText = 'post of current user';
 userContentDown.appendChild(but);
+let boxOfTitlePosts = document.createElement("div");
+boxOfTitlePosts.classList.add('boxOfTitlePosts');
+
+but.onclick = (() =>{
+
+    if (i === 0){
+
+        fullContent.appendChild(boxOfTitlePosts);
+        userContentDown.before(boxOfTitlePosts);
+
+        fetch(`https://jsonplaceholder.typicode.com/users/${idOfUser}/posts`)
+            .then((response) => response.json())
+            .then((users) => {
+                for (const user of users) {
+
+                    let titlePosts = document.createElement("div");
+                    titlePosts.classList.add('titlePosts');
+
+                    boxOfTitlePosts.appendChild(titlePosts);
+
+                    titlePosts.innerHTML = `<h4>Post:</h4> <p><b>${user.title}</b></p>`;
+
+                    let button = document.createElement("button");
+                    button.classList.add('buttonDetails');
+                    button.innerText = 'details';
+                    titlePosts.appendChild(button);
+
+                    button.onclick = () => {
+                        location.href = `user-details.html?id=${user.id}`;
+                    };
+
+                }
+
+            });
+
+    i++;
+    }
+    else  if (i === 1){
+        boxOfTitlePosts.style.display = 'none';
+        i++;
+    }
+    else {
+        boxOfTitlePosts.style.display = 'flex';
+        i--;
+    }
+
+
+        });
+
 
 
 
@@ -82,5 +166,6 @@ userContentDown.appendChild(but);
 // блоки с краткой информацией про post - в ряд по 5 объектов.
 // post-details.html - блок с информацией про пост вверху. Комментарии - по 4 в ряд.
 // Все без исключения элементы, который характеризируют user,post,comment  визуализировать, так, что бы было видно их блоки (дать задний фон + margin. Иными словами - крайне четкая сетка)
+
 
 
